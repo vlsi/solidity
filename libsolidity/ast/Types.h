f96ebd180f34ea0384a810a1dc1b78f0b6e2e8c5
Alex Beregszaszi <alex@rtfs.hu>
2016-11-18 00:41:35 +0000

    Support view/pure FunctionType

--- libsolidity/ast/Types.h
+++ libsolidity/ast/Types.h
@@ -833,7 +833,8 @@ public:
 		strings const& _returnParameterTypes,
 		Location _location = Location::Internal,
 		bool _arbitraryParameters = false,
-		bool _constant = false,
+		bool _view = false,
+		bool _pure = false,
 		bool _payable = false
 	): FunctionType(
 		parseElementaryTypeVector(_parameterTypes),
@@ -843,7 +844,8 @@ public:
 		_location,
 		_arbitraryParameters,
 		nullptr,
-		_constant,
+		_view,
+		_pure,
 		_payable
 	)
 	{
